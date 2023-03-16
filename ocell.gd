8517cf9385eb92863f22daed05d6ceff6be55e57
JordiPiera <127503124+JordiPiera@users.noreply.github.com>
2023-03-16 10:50:09 +0100

    Últims retocs

--- ocell.gd
+++ ocell.gd
@@ -3,10 +3,31 @@ extends RigidBody2D
 var velocitat = 300
 var accio = true
 var contador = 0
+var comenca = false
+
+
+
+func start(state: Physics2DDirectBodyState) -> void:
+	if comenca == false:
+		gravity_scale = -0.001
+		state.linear_velocity.y = 0
+	
+	
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			comenca = true
+			gravity_scale = 8
+
 
 func volar(state: Physics2DDirectBodyState):
-	state.linear_velocity = Vector2.UP * velocitat
-	$AnimatedSprite.look_at($amunt.global_position)
+	if comenca == true:
+		state.linear_velocity = Vector2.UP * velocitat
+		$AnimatedSprite.look_at($amunt.global_position)
+		
+		
+#	state.linear_velocity = Vector2.UP * velocitat
+#	$AnimatedSprite.look_at($amunt.global_position)
 
 func contador():
 	contador += 1
@@ -19,16 +40,23 @@ func die():
 
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-	if state.linear_velocity.y > velocitat * 0.5:
-		$AnimatedSprite.rotation = lerp_angle($AnimatedSprite.rotation,$abaix.position.normalized().angle(),0.1)
+	start(state)
+	if comenca == true:
+		if state.linear_velocity.y > velocitat * 0.5:
+			$AnimatedSprite.rotation = lerp_angle($AnimatedSprite.rotation,$abaix.position.normalized().angle(),0.1)
+		if Input.is_action_just_pressed("volar"):
+			volar(state)
 
-	if Input.is_action_just_pressed("volar") && accio:
-		volar(state)
+#	if state.linear_velocity.y > velocitat * 0.5:
+#		$AnimatedSprite.rotation = lerp_angle($AnimatedSprite.rotation,$abaix.position.normalized().angle(),0.1)
+#	if Input.is_action_just_pressed("volar"):
+#			volar(state)
+
+
 
 func _on_Area2D_body_entered(body):
 	if body.name == "ocell":
 		body.die()
-
 
 func _on_AreaFinal_body_entered(body):
 	if body.name == "ocell":
--- tuberia.gd
+++ tuberia.gd
@@ -1,9 +1,24 @@
 extends Node2D
-var velocitat = 3
+var velocitat = 2
+var comenca = false
 
+
+
+#func _ready():
+#	var button = Button.new()
+#	button.text = "volar"
+#	button.pressed.connect(self._button_pressed)
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			comenca = true
 
 func _physics_process(delta:float) -> void:
-	position += Vector2.LEFT * velocitat
+	if comenca == true:
+		position += Vector2.LEFT * velocitat
+#	position += Vector2.LEFT * velocitat
 
 func _on_tubs_body_entered(body):
 	if body.name == "ocell":
